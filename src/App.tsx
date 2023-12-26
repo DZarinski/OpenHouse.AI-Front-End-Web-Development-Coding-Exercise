becf81fda05af7a948217ed844a42cfc69538cca
David Zarinski <david.zarinski@gmail.com>
2023-12-26 14:20:29 -0700

    Create new app with Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, ChakraProvider, Grid, theme } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Home } from "./pages";
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <Home />
+      </Grid>
+    </Box>
+  </ChakraProvider>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export const Home = () => {
+  return (
+    <Box padding="6" boxShadow="lg">
+      <Heading as="h2" size="xl">
+        Welcome to the Home Page
+      </Heading>
+    </Box>
+  );
+};
