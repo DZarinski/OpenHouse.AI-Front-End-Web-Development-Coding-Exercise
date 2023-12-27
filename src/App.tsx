ba689293c5a8ab2bb4cba8f58c76c0070dab663f
David Zarinski <david.zarinski@gmail.com>
2023-12-26 21:45:43 -0700

    Refactor App and PageWrapper components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { ChakraProvider, Flex, VStack, theme } from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Communities } from "./pages";
 
 const queryClient = new QueryClient();
@@ -8,12 +7,7 @@ const queryClient = new QueryClient();
 export const App = () => (
   <ChakraProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
-      <VStack textAlign="center" fontSize="xl">
-        <Flex justifyContent="flex-end" w="100%">
-          <ColorModeSwitcher justifySelf="flex-end" mr="5" mt="2" />
-        </Flex>
-        <Communities />
-      </VStack>
+      <Communities />
     </QueryClientProvider>
   </ChakraProvider>
 );
--- src/components/layouts/PageWrapper.tsx
+++ src/components/layouts/PageWrapper.tsx
@@ -1,26 +1,37 @@
-import { Box, BoxProps, ContainerProps } from "@chakra-ui/react";
-import React, { PropsWithChildren } from "react";
+import {
+  Container,
+  ContainerProps,
+  VStack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { PropsWithChildren } from "react";
+import { ColorModeSwitcher } from "../../ColorModeSwitcher";
 
-type PageWrapperProps = BoxProps & {
-  footer?: React.ReactNode;
+type PageWrapperProps = ContainerProps & {
   containerProps?: ContainerProps;
 };
 
 export const PageWrapper = ({
   children,
-  footer,
-  ...boxProps
+  ...containerProps
 }: PropsWithChildren<PageWrapperProps>) => {
   return (
-    <Box
-      maxW="7xl"
-      w="full"
-      mx="auto"
-      px={{ base: "4", md: "8", lg: "12" }}
-      py={{ base: "2", md: "4", lg: "4" }}
-      {...boxProps}
+    <VStack
+      textAlign="center"
+      fontSize="xl"
+      backgroundColor={useColorModeValue("gray.50", "gray.900")}
+      height="100vh"
     >
-      {children}
-    </Box>
+      <ColorModeSwitcher alignSelf="flex-end" mr="5" mt="2" />
+
+      <Container
+        maxWidth="7xl"
+        px={{ base: "4", md: "8", lg: "12" }}
+        py={{ base: "2", md: "4", lg: "4" }}
+        {...containerProps}
+      >
+        {children}
+      </Container>
+    </VStack>
   );
 };
